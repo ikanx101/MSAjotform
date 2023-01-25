f36e26fd64242e70773076275c57d0b85612f83d
ikanx101 <ikanx101@gmail.com>
2023-01-25 15:46:38 +0700

    update

--- algoritma/skrip.R
+++ algoritma/skrip.R
@@ -29,6 +29,16 @@ trans_submission_date = function(temp){
   format(temp,"%d-%m-%Y")
 }
 
+trans_tahun = function(temp){
+  temp = as.Date(temp,"%B %d, %Y")
+  format(temp,"%Y")
+}
+
+trans_bulan = function(temp){
+  temp = as.Date(temp,"%B %d, %Y")
+  format(temp,"%m")
+}
+
 # fungsi untuk bikin judul proper
 proper_new = function(x){
   tess = stringi::stri_trans_general(x,id = "Title")
@@ -37,10 +47,11 @@ proper_new = function(x){
 
 # ==========================================================
 # kita mulai
-df %>% 
+df_final = 
+  df %>% 
   rowwise() %>% 
-  mutate(submission_date = trans_submission_date(submission_date),
-         date            = trans_submission_date(date)) %>% 
+  mutate(tahun           = trans_tahun(submission_date),
+         bulan           = trans_bulan(submission_date)) %>% 
   ungroup() %>% 
   separate(provinsi_kota_kabupaten,
            into = c("provinsi","kota_kab"),
@@ -58,9 +69,49 @@ df %>%
            sep  = ";") %>% 
   mutate(area_rumah_sakit = stringr::str_trim(area_rumah_sakit),
          nama_rumah_sakit_jika_belum_ada_hub_okky = stringr::str_trim(nama_rumah_sakit_jika_belum_ada_hub_okky)) %>% 
-  View()
+  rename(nama_outlet = nama_outlet_horeka_rumah_sakit_umkm_gym_atau_instansi,
+         jabatan_pic_outlet = jabatan) %>% 
+  select(bulan,tahun,provinsi,kota_kab,dept,pic,jenis_kunjungan,nama_outlet,channel,category,
+         terdapat_sugar_display_condiment_bar_atau_sugar_bowl,outlet_approach_project_item_bulk,
+         termasuk_specialty_coffee_shop_roastery_terdapat_manual_brew_atau_banyak_kreasi_menu_kopi,
+         google_rate,google_review,status_brand_tropicana_slim,status_brand_nutrisari,
+         status_brand_hi_lo,status_brand_lokalate,status_brand_l_men,
+         jabatan_pic_outlet,nama_pic,penjualan,notes) 
 
+df_final = 
+  df_final %>% 
+  mutate(id = 1:nrow(df_final)) %>% 
+  relocate(id,.before = bulan) %>% 
+  separate_rows(penjualan,
+                sep = "\n") %>% 
+  filter(!grepl("total",penjualan,ignore.case = T)) %>% 
+  separate(penjualan,
+           into = c("item","dummy"),
+           sep = "\\(Amount:") %>% 
+  separate(dummy,
+           into = c("price","quantity"),
+           sep = "IDR, Jumlah:") %>% 
+  mutate(quantity = gsub("\\)","",quantity),
+         item = stringr::str_trim(item),
+         price = stringr::str_trim(price),
+         price = gsub("\\,","",price),
+         price = as.numeric(price),
+         quantity = stringr::str_trim(quantity),
+         quantity = as.numeric(quantity),
+         total_value = price * quantity) %>% 
+  rowwise() %>% 
+  mutate(brand = case_when(
+    grepl("ts|tropica",item,ignore.case = T) ~ "TS",
+    grepl("lmen|l men|l-men",item,ignore.case = T) ~ "L-Men",
+    grepl("ns|nutri",item,ignore.case = T) ~ "NS",
+    grepl("lokal",item,ignore.case = T) ~ "Lokalate",
+    grepl("Diabetamil",item,ignore.case = T) ~ "Diabetamil",
+    grepl("hilo|hi lo",item,ignore.case = T) ~ "HILO"
+  )) %>% 
+  ungroup() %>% 
+  mutate(area = "BALI IBT") %>% 
+  relocate(area,.before = bulan)
 
-
+openxlsx::write.xlsx(df_final,file = "output.xlsx")
 
 
